0ff8e70852e94f8627172f8e85c81c928b3379a2
toukubo <toukubo@gmail.com>
2022-06-02 18:56:32 +0900

    empty row skipped

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 args = require('minimist')(process.argv.slice(2))
 word = args.word
 const sh = require('shelljs')
-console.log("args : ")
-console.dir(args)
-
-console.log("word : ")
-console.dir(word)
+// console.dir("args : " + args)
+// console.log("word : "+ word)
 
 var inputText = "";
 process.stdin.on('data', function(chunk){
   inputText += chunk;
 });
 
-// ストリーム終了時に呼ばれる.
 urls = new Array()
 
 process.stdin.on('end', function(){
   inputText.split('\n').forEach(function(url){
-    if(inputText){
-      console.log("line : " + url)
+    if(inputText && url){
+      // console.log("line : " + url)
       urls.push(url)
     }
   });
-  console.log("urls : ")
-  console.dir(urls)
+  // console.log("urls: " +urls)
   
   urls.forEach(function(url){
-    command = "pocket-cli add --url " + url
+    command = "pocket-cli add --url " + "'" + url + "'"
     sh.exec(command,{async:true})
-    console.log("command : ")
-    console.dir(command)
+    // console.log("command : "+command)
   
   });
 });
